ed365b23ce2607f50b18d170850e4db1d4d33b8e
Anthony Jarrett <anthonydgjarrett@gmail.com>
2020-03-14 20:49:27 -0400

    use agent lists

--- python/src/unpackaged/abm/model.py
+++ python/src/unpackaged/abm/model.py
@@ -1,68 +1,72 @@
 import random
 
-# Point 0
+agents = []
 
-# Set up variables
-y0 = random.randint(0, 99)
-x0 = random.randint(0, 99)
+# Set up agents
+agents.append([random.randint(0, 99), random.randint(0, 99)])
+agents.append([random.randint(0, 99), random.randint(0, 99)])
 
-# Random walk one step
-if random.random() < 0.5:
-    y0 += 1
-else:
-    y0 -= 1
+print(agents)
 
-if random.random() < 0.5:
-    x0 += 1
-else:
-    x0 -= 1
+
+# Move point 0
 
 # Random walk one step
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
 
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
-
-print(y0, x0)
-
-
-# Point 1
-
-# Set up variables
-y1 = random.randint(0, 99)
-x1 = random.randint(0, 99)
+    agents[0][1] -= 1
 
 # Random walk one step
 if random.random() < 0.5:
-    y1 += 1
+    agents[0][0] += 1
 else:
-    y1 -= 1
+    agents[0][0] -= 1
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[0][1] += 1
 else:
-    x1 -= 1
+    agents[0][1] -= 1
+
+print(agents[0][0], agents[0][1])
+
+
+# Move point 1
 
 # Random walk one step
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
-print(y1, x1)
+# Random walk one step
+if random.random() < 0.5:
+    agents[1][0] += 1
+else:
+    agents[1][0] -= 1
+
+if random.random() < 0.5:
+    agents[1][1] += 1
+else:
+    agents[1][1] -= 1
+
+print(agents[1][0], agents[1][1])
 
 
 # Calculate Pythagorian distance between point 0 and point 1
-distance = ((y1 - y0)**2 + (x1 - x0)**2 )**0.5
+distance = (
+        (agents[1][0] - agents[0][0])**2 + 
+        (agents[1][1] - agents[0][1])**2 
+)**0.5
 
 print(distance)
