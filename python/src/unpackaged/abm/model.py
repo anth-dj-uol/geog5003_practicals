282bfa54a568a79fc5494c8b0845ebdd664b7a17
Anthony Jarrett <AnthonyJarrett@nanometrics.ca>
2020-04-11 23:23:33 -0400

    animate model using num_of_iterations

--- python/src/unpackaged/abm/model.py
+++ python/src/unpackaged/abm/model.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot
+import matplotlib.animation 
 import agentframework
 import csv
 import random
 
 num_of_agents = 50
-num_of_iterations = 100
+num_of_iterations = 200
 neighbourhood = 20
-
 filename = 'in.txt'
 
 agents = []
 environment = []
+
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+ax.set_autoscale_on(False)
+
 
 with open(filename, newline='') as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
@@ -20,23 +25,23 @@ with open(filename, newline='') as f:
             rowlist.append(value)
         environment.append(rowlist)
 
-# Set up agents (y, x)
+# Set up agents
 for i in range(num_of_agents):
     agents.append(agentframework.Agent(environment, agents))
 
 
-# Move agent
-for _ in range(num_of_iterations):
+def update(frame_number):
+    fig.clear()
+    random.shuffle(agents)
     for i in range(num_of_agents):
-        random.shuffle(agents)
         agents[i].move()
         agents[i].eat()
         agents[i].share_with_neighbours(neighbourhood)
+    matplotlib.pyplot.ylim(0, 99)
+    matplotlib.pyplot.xlim(0, 99)
+    matplotlib.pyplot.imshow(environment)
+    for agent in agents:
+        matplotlib.pyplot.scatter(agent.x, agent.y, color='black')
 
-
-matplotlib.pyplot.ylim(0, 99)
-matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.imshow(environment)
-for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i].x, agents[i].y, color='black')
+animation = matplotlib.animation.FuncAnimation(fig, update, interval=50, repeat=False, frames=num_of_iterations)
 matplotlib.pyplot.show()
