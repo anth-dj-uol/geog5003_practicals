50ab2bb48342e66da5468f3d72afbffba965624b
Anthony Jarrett <AnthonyJarrett@nanometrics.ca>
2020-04-26 23:56:01 -0400

    start simulation in main function

--- python/src/unpackaged/abm/model.py
+++ python/src/unpackaged/abm/model.py
@@ -34,6 +34,10 @@ fig = matplotlib.pyplot.figure(figsize=(7, 7))
 ax = fig.add_axes([0, 0, 1, 1])
 ax.set_autoscale_on(False)
 
+# Initialize variables
+agents = []
+environment = []
+
 def fetch_start_positions(url):
     """
     Return the start positions from the provided URL.
@@ -46,11 +50,11 @@ def fetch_start_positions(url):
     td_ys = soup.find_all(attrs={"class" : "y"})
     return (td_xs, td_ys)
 
-def populate_environment(filename):
+def populate_environment(filename, environment):
     """
     Return environment data from the given file path.
     """
-    environment = []
+    environment = [] if environment is None else environment
     with open(filename, newline='') as f:
         reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
         for row in reader:
@@ -94,33 +98,39 @@ def update(frame_number, move=True):
         matplotlib.pyplot.scatter(agent.x, agent.y, color='black')
 
 
-def run():
+def run(canvas):
     """
     Start the simulation in an animated figure.
     """
     animation = matplotlib.animation.FuncAnimation(fig, update, interval=10, repeat=False, frames=num_of_iterations)
     canvas.draw()
 
-# Setup model
-agents = []
-environment = populate_environment(default_filename)
-start_positions = fetch_start_positions(default_start_positions_url)
-setup_agents(default_num_of_agents, environment, agents, start_positions)
 
-# Create GUI window
-root = tkinter.Tk()
-root.wm_title("Agent-Based Model")
-canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
-canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+def main():
 
-# Create menu
-menubar = tkinter.Menu(root)
-root.config(menu=menubar)
-model_menu = tkinter.Menu(menubar)
-menubar.add_cascade(label="Model", menu=model_menu)
-model_menu.add_command(label="Run model", command=run)
+    # Setup model
+    populate_environment(default_filename, environment)
+    start_positions = fetch_start_positions(default_start_positions_url)
+    setup_agents(default_num_of_agents, environment, agents, start_positions)
 
-# Display initial model
-update(0, False)
+    # Create GUI window
+    root = tkinter.Tk()
+    root.wm_title("Agent-Based Model")
+    canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+    canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-#root.mainloop()
+    # Create menu
+    menubar = tkinter.Menu(root)
+    root.config(menu=menubar)
+    model_menu = tkinter.Menu(menubar)
+    menubar.add_cascade(label="Model", menu=model_menu)
+    model_menu.add_command(label="Run model", command= lambda: run(canvas))
+
+    # Display initial model
+    update(0, False)
+
+    #root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
